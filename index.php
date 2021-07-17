d6badd3d407a283fe4563d2773bbf12132406c22
usuryadevara <32099886+usuryadevara@users.noreply.github.com>
2021-07-17 10:20:20 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <html lang = "en">
    
    <head>
-      <title>Tutorialspoint.com</title>
+      <title>Prod Tutorialspoint.com</title>
       <link href = "css/bootstrap.min.css" rel = "stylesheet">
       
       <style>
@@ -75,7 +75,7 @@
 	
    <body>
       
-      <h2>Enter Username and Password</h2> 
+      <h2>This is Prod, Enter Username and Password</h2> 
       <div class = "container form-signin">
          
          <?php
